8c83ba1eaea7694429afc57a2381e3c285dabad4
Vaeth <fyrexbright@gmail.com>
2020-10-04 17:55:32 -0500

    P1A Done

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text currentScoreTextView;
+    int currentScore;
+
+
+    private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        currentScore += scoreIncrease;
+        currentScoreTextView.text = "Score: " + currentScore.ToString();
+    }
+
+
+
+    public void ExitLevel()
+    {
+        int highscore = PlayerPrefs.GetInt("HighScore");
+        if(currentScore>highscore)
+        {
+            PlayerPrefs.SetInt("HighScore", currentScore);
+            Debug.Log("New high score: " + currentScore);
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text highScoreTextView;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        highScoreTextView.text = highScore.ToString();
+        
+            if(_startingSong != null)
+            {
+                AudioManager.Instance.PlaySong(_startingSong);
+            }
+        
+    }
+
+
+
+
+
+}
